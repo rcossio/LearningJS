be4c5446c1d10f39fd8049d91afe8a5f85175d6a
rcossio <rodrigoperez93@gmail.com>
2022-07-27 21:49:22 -0300

    First functional template, I still lack the use of a function

--- js/code.js
+++ js/code.js
@@ -1,2 +1,22 @@
+let meal;
+let mealCounter = 0;
 
+
+
+do {
+    meal = prompt("Write one by one the name of your meals (max 5, leave empty to exit)");
+
+    if (meal == "") {
+        alert("You didn't enter a meal, exiting...");
+    } else {
+        document.getElementsByClassName("meal-span")[mealCounter].textContent=meal;
+        console.log("You entered the meal "+meal);
+        if (mealCounter >= 4){
+            alert("Good job! You entered all 5 meals.");
+            break
+        }
+    }
+
+    mealCounter++
+} while (meal != "");
 
