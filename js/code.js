987746301a41e98999e84f03e03daf0ace4bc91d
rcossio <rodrigoperez93@gmail.com>
2022-08-01 20:48:37 -0300

    Adding arrays and higher order functions over them

--- js/code.js
+++ js/code.js
@@ -1,75 +1,47 @@
-
 let meal;
 let mealCounter = 0;
-let meal1;
-let meal2;
-let meal3;
-let meal4;
-let meal5;
-let meal6;
-let meal7;
-const totalMacros = { 
-    proteins:0,
-    fats:0,
-    carbs:0,
-    calories: function () {return (this.proteins+this.carbs)*4+this.fats*9; } 
-    }
+const mealList = [];
+const totalMacros = {
+    proteins: 0,
+    fats: 0,
+    carbs: 0,
+    calories: function () { return (this.proteins + this.carbs) * 4 + this.fats * 9; }
+}
 
-function mealChecker (meal){
-    /* Returns true if input is acceptable (meal/empty or cancel button) */
+function mealChecker(meal) {
+    /* Returns true if input is acceptable (meal,empty or cancel button) */
+    let mealNameArray = mealList.map( (item) => item.name )
+    mealNameArray.push("")
     if (meal == null) {
         return true
-    }
-    switch (meal.toLowerCase()) {
-        case "guiso":
-        case "tacos":
-        case "lasagna":
-        case "pizza":
-        case "ravioles":
-        case "ensalada":
-        case "fideos":
-        case "":
-            return true;
-        default:
-            return false;
+    } else if (mealNameArray.includes(meal.toLowerCase())) {
+        return true
+    } else {
+        return false
     }
 };
 
-function Meal(name,proteins,fats,carbs){
-    /* Constructor for the object meal */
-    this.name = name;
-    this.proteins = proteins;
-    this.fats = fats;
-    this.carbs = carbs;
+class Meal {
+    constructor (name, proteins, fats, carbs) {
+        this.name = name;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.carbs = carbs;}
 }
 
-function getMealObj(meal){
-    switch (meal.toLowerCase()) {
-        case "guiso":
-            return meal1;
-        case "tacos":
-            return meal2;
-        case "lasagna":
-            return meal3;
-        case "pizza":
-            return meal4;
-        case "ravioles":
-            return meal5;
-        case "ensalada":
-            return meal6;
-        case "fideos":
-            return meal7;
-    }
+function getMealObj(meal) {
+    /* Returns meal object by finding it from its name */
+    return mealList.find((elem) => elem.name === meal)
 }
 
 /* Creating (fake) meals. This should be read from a table or database in the future*/
-meal1 = new Meal("guiso",11,21,31)
-meal2 = new Meal("tacos",12,22,32)
-meal3 = new Meal("lasagna",13,23,33)
-meal4 = new Meal("pizza",14,24,34)
-meal5 = new Meal("ravioles",15,25,35)
-meal6 = new Meal("ensalada",16,26,36)
-meal7 = new Meal("fideos",17,27,37)
+mealList.push( new Meal("guiso", 11, 21, 31)   )
+mealList.push( new Meal("tacos", 12, 22, 32)   )
+mealList.push( new Meal("lasagna", 13, 23, 33) )
+mealList.push( new Meal("pizza", 14, 24, 34)   )
+mealList.push( new Meal("ravioles", 15, 25, 35))
+mealList.push( new Meal("ensalada", 16, 26, 36))
+mealList.push( new Meal("fideos", 17, 27, 37)  )
 
 
 /* Reading the input*/
@@ -80,11 +52,10 @@ do {
     if ((meal == "") || (meal == null)) {
         alert("You didn't enter a meal, exiting...");
         break
-    } 
-    else if (passed) {
+    } else if (passed) {
         mealCounter++
         mealObj = getMealObj(meal)
-        console.log("You entered the meal "+meal+". With "+mealObj.proteins+" proteins, "+mealObj.fats+" fats, and "+mealObj.carbs+" carbs.");
+        console.log("You entered the meal " + meal + ". With " + mealObj.proteins + " proteins, " + mealObj.fats + " fats, and " + mealObj.carbs + " carbs.");
 
         /* Calculating total macros*/
         totalMacros.proteins += mealObj.proteins;
@@ -92,22 +63,21 @@ do {
         totalMacros.carbs += mealObj.carbs;
 
         /* Parte que googlee de ansioso pero que aun no vimos */
-        document.getElementsByClassName("name-span")[mealCounter-1].textContent=mealObj.name;
-        document.getElementsByClassName("proteins-span")[mealCounter-1].textContent=mealObj.proteins;
-        document.getElementsByClassName("fats-span")[mealCounter-1].textContent=mealObj.fats;
-        document.getElementsByClassName("carbs-span")[mealCounter-1].textContent=mealObj.carbs;
+        document.getElementsByClassName("name-span")[mealCounter - 1].textContent = mealObj.name;
+        document.getElementsByClassName("proteins-span")[mealCounter - 1].textContent = mealObj.proteins;
+        document.getElementsByClassName("fats-span")[mealCounter - 1].textContent = mealObj.fats;
+        document.getElementsByClassName("carbs-span")[mealCounter - 1].textContent = mealObj.carbs;
 
-        if (mealCounter >= 5){
+        if (mealCounter >= 5) {
             alert("Good job! You entered all 5 meals.");
             break
         }
-    } 
-    else {
+    } else {
         alert("You didn't enter a valid meal, try again.");
         continue
     }
 
-    
+
 } while (meal != "");
 
-alert("The quantity of calories in this plan is "+totalMacros.calories()+" with "+totalMacros.proteins+" proteins, "+totalMacros.fats+" fats, and "+totalMacros.carbs+" carbs.")
+alert("The quantity of calories in this plan is " + totalMacros.calories() + " with " + totalMacros.proteins + " proteins, " + totalMacros.fats + " fats, and " + totalMacros.carbs + " carbs.")
