76cf6b016d6130ec2baa82f970fb159a2b216e5c
rcossio <rodrigoperez93@gmail.com>
2022-08-10 15:58:41 -0300

    Event button added, JS script is dirty but functional

--- js/code.js
+++ js/code.js
@@ -1,33 +1,48 @@
 let meal;
-let mealCounter = 0;
 const mealList = [];
-const totalMacros = {
-    /* proteins, fats and carbs are Per Serving Size */
-    proteins: 0,
-    fats: 0,
-    carbs: 0,
-    calories: function () { return (this.proteins + this.carbs) * 4 + this.fats * 9; }
-}
 
+
+/* Defines a new method to transform string to lowercase with first char in uppercase  */
+String.prototype.firstToUpperCase = function () {
+	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+/* Returns true if input is acceptable (meal or empty) */
 function mealChecker(meal) {
-    /* Returns true if input is acceptable (meal or empty) */
-    if (mealNameArray.includes(meal.toLowerCase())) {
+    if (mealNameArray.includes(meal.firstToUpperCase())) {
         return true
     } else {
         return false
     }
 };
 
+/* Returns meal object by finding it from its name */
 function getMealObj(meal) {
-    /* Returns meal object by finding it from its name */
-    return mealList.find((elem) => elem.name === meal.toLowerCase())
+    return mealList.find((elem) => elem.name === meal.firstToUpperCase())
 }
 
+/* Used to established a dash where the input is invalid */
 function setInvalidMeal(index){
-    /* Used to established a dash where the input is invalid */
     document.getElementsByClassName("proteins-span")[index].textContent = "-";
     document.getElementsByClassName("fats-span")[index].textContent = "-";
     document.getElementsByClassName("carbs-span")[index].textContent = "-";
+}
+
+class totalMacros {
+    constructor (){
+        /* proteins, fats and carbs are Per Serving Size */
+        this.proteins= 0;
+        this.fats=0;
+        this.carbs=0;
+    }
+
+    calories() { return (this.proteins + this.carbs) * 4 + this.fats * 9; };
+
+    resetValues() {
+        this.proteins=0;
+        this.fats=0;
+        this.carbs=0;
+    }
 }
 
 class Meal {
@@ -42,23 +57,23 @@ class Meal {
 
     setServings(servings){
         this.servings = servings;
-    }
+    };
 
-    totalProteins(){ return this.proteins*this.servings; }
+    totalProteins(){ return this.proteins*this.servings; };
 
-    totalFats(){ return this.fats*this.servings; }
+    totalFats(){ return this.fats*this.servings; };
 
-    totalCarbs(){ return this.carbs*this.servings; }
+    totalCarbs(){ return this.carbs*this.servings; };
 }
 
 /* Creating (fake) meals. This should be read from a table or database in the future*/
-mealList.push( new Meal("guiso", 11, 21, 31)   )
-mealList.push( new Meal("tacos", 12, 22, 32)   )
-mealList.push( new Meal("lasagna", 13, 23, 33) )
-mealList.push( new Meal("pizza", 14, 24, 34)   )
-mealList.push( new Meal("ravioles", 15, 25, 35))
-mealList.push( new Meal("ensalada", 16, 26, 36))
-mealList.push( new Meal("fideos", 17, 27, 37)  )
+mealList.push( new Meal("Guiso", 11, 21, 31)   )
+mealList.push( new Meal("Tacos", 12, 22, 32)   )
+mealList.push( new Meal("Lasagna", 13, 23, 33) )
+mealList.push( new Meal("Pizza", 14, 24, 34)   )
+mealList.push( new Meal("Ravioles", 15, 25, 35))
+mealList.push( new Meal("Ensalada", 16, 26, 36))
+mealList.push( new Meal("Fideos", 17, 27, 37)  )
 
 /* Defines mealNameArray, which is the array of possible input meals*/
 /* It is defined as a global variable instead of inside mealChecker so it wont be defined on every prompt */
@@ -80,35 +95,45 @@ if (!accepted) {
     throw new Error("The user didn't accept to run the script");
 }
 
-/* Reading the input*/
-for (let mealInput of document.getElementsByClassName("meal-selection")) {
-    let meal = mealInput.value;
-    let passed = mealChecker(meal);
+/* Use click on button as and event (lo vimos en el afterclass del miercoles) */
+let eventButton = document.getElementById("calculateMacros-btn");
+eventButton.onclick = calculateMacros;
 
-    mealCounter++;
-
-    if ((meal == "") || (!passed)) {
-        mealInput.style.background = "rgba(255,0,0,0.3)"
-        setInvalidMeal(mealCounter - 1);
-        continue;
+/* Reading the data and calculating*/
+function calculateMacros(){
+    let mealCounter = 0;
+    let totalMacrosInPlan = new totalMacros();
+    for (let mealInput of document.getElementsByClassName("meal-selection")) {
+        let meal = mealInput.value;
+        let passed = mealChecker(meal);
+    
+        mealCounter++;
+    
+        if ((meal == "") || (!passed)) {
+            mealInput.style.background = "rgba(255,0,0,0.3)"
+            setInvalidMeal(mealCounter - 1);
+            continue;
+        } else {
+            mealInput.style.background = null
+        }
+        
+        mealObj = getMealObj(meal)
+        mealObj.setServings(document.getElementsByClassName("serving-span")[mealCounter - 1].value);
+    
+        /* Calculating total macros*/
+        totalMacrosInPlan.proteins += mealObj.totalProteins();
+        totalMacrosInPlan.fats += mealObj.totalFats();
+        totalMacrosInPlan.carbs += mealObj.totalCarbs();
+    
+        /* Set nutriend values in the table */
+        document.getElementsByClassName("proteins-span")[mealCounter - 1].textContent = mealObj.totalProteins();
+        document.getElementsByClassName("fats-span")[mealCounter - 1].textContent = mealObj.totalFats();
+        document.getElementsByClassName("carbs-span")[mealCounter - 1].textContent = mealObj.totalCarbs();
+    
+        if (mealCounter >= 5) {
+            break;
+        }
     }
     
-    mealObj = getMealObj(meal)
-    mealObj.setServings(document.getElementsByClassName("serving-span")[mealCounter - 1].value);
-
-    /* Calculating total macros*/
-    totalMacros.proteins += mealObj.totalProteins();
-    totalMacros.fats += mealObj.totalFats();
-    totalMacros.carbs += mealObj.totalCarbs();
-
-    /* Set nutriend values in the table */
-    document.getElementsByClassName("proteins-span")[mealCounter - 1].textContent = mealObj.totalProteins();
-    document.getElementsByClassName("fats-span")[mealCounter - 1].textContent = mealObj.totalFats();
-    document.getElementsByClassName("carbs-span")[mealCounter - 1].textContent = mealObj.totalCarbs();
-
-    if (mealCounter >= 5) {
-        break;
-    }
+    document.getElementById("calories-calculation").textContent = "The quantity of calories in this plan is " + totalMacrosInPlan.calories() + " with " + totalMacrosInPlan.proteins + " proteins, " + totalMacrosInPlan.fats + " fats, and " + totalMacrosInPlan.carbs + " carbs.";
 }
-
-document.getElementById("calories-calculation").textContent = "The quantity of calories in this plan is " + totalMacros.calories() + " with " + totalMacros.proteins + " proteins, " + totalMacros.fats + " fats, and " + totalMacros.carbs + " carbs.";
