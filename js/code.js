d6a1ef1d5b04439d61c599552c505341419dcc50
rcossio <rodrigoperez93@gmail.com>
2022-08-10 17:53:00 -0300

    Reworked JS script to be simpler

--- js/code.js
+++ js/code.js
@@ -1,50 +1,10 @@
-let meal;
-const mealList = [];
 
-
-/* Defines a new method to transform string to lowercase with first char in uppercase  */
+/* Method to transform string to lowercase with first char in uppercase  */
 String.prototype.firstToUpperCase = function () {
 	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
 
-/* Returns true if input is acceptable (meal or empty) */
-function mealChecker(meal) {
-    if (mealNameArray.includes(meal.firstToUpperCase())) {
-        return true
-    } else {
-        return false
-    }
-};
-
-/* Returns meal object by finding it from its name */
-function getMealObj(meal) {
-    return mealList.find((elem) => elem.name === meal.firstToUpperCase())
-}
-
-/* Used to established a dash where the input is invalid */
-function setInvalidMeal(index){
-    document.getElementsByClassName("proteins-span")[index].textContent = "-";
-    document.getElementsByClassName("fats-span")[index].textContent = "-";
-    document.getElementsByClassName("carbs-span")[index].textContent = "-";
-}
-
-class totalMacros {
-    constructor (){
-        /* proteins, fats and carbs are Per Serving Size */
-        this.proteins= 0;
-        this.fats=0;
-        this.carbs=0;
-    }
-
-    calories() { return (this.proteins + this.carbs) * 4 + this.fats * 9; };
-
-    resetValues() {
-        this.proteins=0;
-        this.fats=0;
-        this.carbs=0;
-    }
-}
-
+/* Saves & retrieves information about meals*/
 class Meal {
     constructor (name, proteins, fats, carbs) {
         /* Proteins, fats and carbs are Per Serving Size */
@@ -66,74 +26,93 @@ class Meal {
     totalCarbs(){ return this.carbs*this.servings; };
 }
 
-/* Creating (fake) meals. This should be read from a table or database in the future*/
-mealList.push( new Meal("Guiso", 11, 21, 31)   )
-mealList.push( new Meal("Tacos", 12, 22, 32)   )
-mealList.push( new Meal("Lasagna", 13, 23, 33) )
-mealList.push( new Meal("Pizza", 14, 24, 34)   )
-mealList.push( new Meal("Ravioles", 15, 25, 35))
-mealList.push( new Meal("Ensalada", 16, 26, 36))
-mealList.push( new Meal("Fideos", 17, 27, 37)  )
-
-/* Defines mealNameArray, which is the array of possible input meals*/
-/* It is defined as a global variable instead of inside mealChecker so it wont be defined on every prompt */
-let mealNameArray = mealList.map( (item) => item.name )
-mealNameArray.unshift("")
-
-/* Loads the page with the possible meals */
-let availableMeals = document.getElementById("meals-available");
-for (let mealName of mealNameArray) {
-    let mealNewOption = document.createElement("option");
-    mealNewOption.innerHTML = mealName;
-    availableMeals.append(mealNewOption);
-}
-
-/* Agreement to use test script */
+/* Asks user for permission to run the script */
 let accepted = confirm("This site is not currently working with real nutritional information. Do you still want to enter the site?")
 if (!accepted) {
-    /* Aun no vimos esto pero tambien lo google, no se si hay una mejor forma de terminar el script */
+    /* Aun no vimos esto que googlee, no se si hay una mejor forma de terminar el script */
     throw new Error("The user didn't accept to run the script");
 }
 
-/* Use click on button as and event (lo vimos en el afterclass del miercoles) */
-let eventButton = document.getElementById("calculateMacros-btn");
-eventButton.onclick = calculateMacros;
+/* Creates meals. This will be loaded from JSON file in the future*/
+const mealObjArray = [];
+mealObjArray.push( new Meal("Guiso", 11, 21, 31)   )
+mealObjArray.push( new Meal("Tacos", 12, 22, 32)   )
+mealObjArray.push( new Meal("Lasagna", 13, 23, 33) )
+mealObjArray.push( new Meal("Pizza", 14, 24, 34)   )
+mealObjArray.push( new Meal("Ravioles", 15, 25, 35))
+mealObjArray.push( new Meal("Ensalada", 16, 26, 36))
+mealObjArray.push( new Meal("Fideos", 17, 27, 37)  )
 
-/* Reading the data and calculating*/
+/* Defines available meals to input*/
+let mealNameArray = mealObjArray.map( (item) => item.name )
+mealNameArray.unshift("")
+
+/* Loads the page with available meals */
+let mealDatalist = document.getElementById("meals-available");
+for (let mealName of mealNameArray) {
+    mealDatalist.innerHTML += `<option>${mealName}</option>`;
+}
+
+/* Runs main function by pressing button (lo vimos en el afterclass del miercoles) */
+let calculateButton = document.getElementById("calculateMacros-btn");
+calculateButton.onclick = calculateMacros;
+
+/* Returns true if input is acceptable (meal or empty) */
+function mealChecker(meal) {
+    if (mealNameArray.includes(meal.firstToUpperCase())) {
+        return true
+    } else {
+        return false
+    }
+};
+
+/* Finds meal object from its name */
+function getMealObj(meal) {
+    return mealObjArray.find((elem) => elem.name === meal.firstToUpperCase())
+}
+
+/* Displays dash where the input is invalid */
+function setInvalidMeal(index){
+    document.getElementsByClassName("proteins-span")[index].textContent = "-";
+    document.getElementsByClassName("fats-span")[index].textContent = "-";
+    document.getElementsByClassName("carbs-span")[index].textContent = "-";
+}
+
+/* Reading the data and calculating macronutrients*/
 function calculateMacros(){
-    let mealCounter = 0;
-    let totalMacrosInPlan = new totalMacros();
+
+    let index = 0;
+    const totalMacros = { proteins: 0, fats:0, carbs:0, 
+        calories: function () { return (this.proteins + this.carbs) * 4 + this.fats * 9; }
+    }
+
     for (let mealInput of document.getElementsByClassName("meal-selection")) {
         let meal = mealInput.value;
         let passed = mealChecker(meal);
     
-        mealCounter++;
-    
+        /* Skips when input is invalid */
         if ((meal == "") || (!passed)) {
             mealInput.style.background = "rgba(255,0,0,0.3)"
-            setInvalidMeal(mealCounter - 1);
+            setInvalidMeal(index);
+            index++;
             continue;
-        } else {
-            mealInput.style.background = null
         }
+        mealInput.style.background = null
         
+        /* Sums up macronutrients*/
         mealObj = getMealObj(meal)
-        mealObj.setServings(document.getElementsByClassName("serving-span")[mealCounter - 1].value);
+        mealObj.setServings(document.getElementsByClassName("serving-span")[index].value);
+        totalMacros.proteins += mealObj.totalProteins();
+        totalMacros.fats += mealObj.totalFats();
+        totalMacros.carbs += mealObj.totalCarbs();
     
-        /* Calculating total macros*/
-        totalMacrosInPlan.proteins += mealObj.totalProteins();
-        totalMacrosInPlan.fats += mealObj.totalFats();
-        totalMacrosInPlan.carbs += mealObj.totalCarbs();
+        /* Displays values in the table */
+        document.getElementsByClassName("proteins-span")[index].textContent = mealObj.totalProteins();
+        document.getElementsByClassName("fats-span")[index].textContent = mealObj.totalFats();
+        document.getElementsByClassName("carbs-span")[index].textContent = mealObj.totalCarbs();
     
-        /* Set nutriend values in the table */
-        document.getElementsByClassName("proteins-span")[mealCounter - 1].textContent = mealObj.totalProteins();
-        document.getElementsByClassName("fats-span")[mealCounter - 1].textContent = mealObj.totalFats();
-        document.getElementsByClassName("carbs-span")[mealCounter - 1].textContent = mealObj.totalCarbs();
-    
-        if (mealCounter >= 5) {
-            break;
-        }
+        index++;
     }
     
-    document.getElementById("calories-calculation").textContent = "The quantity of calories in this plan is " + totalMacrosInPlan.calories() + " with " + totalMacrosInPlan.proteins + " proteins, " + totalMacrosInPlan.fats + " fats, and " + totalMacrosInPlan.carbs + " carbs.";
+    document.getElementById("calories-calculation").textContent = "The quantity of calories in this plan is " + totalMacros.calories() + " with " + totalMacros.proteins + " proteins, " + totalMacros.fats + " fats, and " + totalMacros.carbs + " carbs.";
 }
